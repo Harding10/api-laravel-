0b82914c749c3d58290be1b45110f893f2d50efb
Harding10 <behdegryjeremieharding@gmail.com>
2025-10-24 01:22:39 +0000

    mise a jour

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,20 +12,21 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * Le mot de passe actuel utilisé par la factory.
      */
     protected static ?string $password;
 
     /**
-     * Define the model's default state.
+     * Définir l'état par défaut du modèle.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
+        // CORRECTION: Utilisation de $this->faker pour accéder aux méthodes de Faker.
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
@@ -33,7 +34,7 @@ class UserFactory extends Factory
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indiquer que l'adresse e-mail du modèle ne doit pas être vérifiée.
      */
     public function unverified(): static
     {
